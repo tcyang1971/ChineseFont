2d5a79fad3bfb94e98d72877de2a8a69e0807398
tcyang <tcyang@gm.pu.edu.tw>
2021-04-13 14:59:01 +0800

    顯示中文字體

--- app/src/main/java/tw/edu/pu/csim/tcyang/chinesefont/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/chinesefont/MainActivity.kt
@@ -1,11 +1,18 @@
 package tw.edu.pu.csim.tcyang.chinesefont
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var txv: TextView = findViewById(R.id.txv)
+        txv.text = "新蒂手指畫字體"
+        txv.setTypeface(Typeface.createFromAsset(assets,
+                "font/HanyiSentyFingerPainting.ttf"))
     }
 }
